ca8c5b401b932748e0e35bf793503a8790641439
mike101102 <mjorgensenlv@gmail.com>
2014-03-30 11:32:44 -0700

    Formatting and return to v.3

--- src/com/mike101102/ctt/CTTTeam.java
+++ src/com/mike101102/ctt/CTTTeam.java
@@ -5,15 +5,15 @@ import com.mike101102.ctt.gameapi.GameTeam;
 public class CTTTeam extends GameTeam {
 
     private int blocks = 0;
-    
+
     public CTTTeam(String name) {
         super(name);
     }
-    
+
     public int getBlocks() {
         return blocks;
     }
-    
+
     public void setBlocks(int blocks) {
         this.blocks = blocks;
     }
--- src/com/mike101102/ctt/PlayerStats.java
+++ src/com/mike101102/ctt/PlayerStats.java
@@ -7,10 +7,10 @@ public class PlayerStats {
     private int losses;
     private int kills;
     private int deaths;
-    
+
     /**
      * Only provide what has changed, everything else set to 0 for no change
-     *
+     * 
      * @param name
      * @param wins
      * @param losses
@@ -24,23 +24,23 @@ public class PlayerStats {
         this.kills = kills;
         this.deaths = deaths;
     }
-    
+
     public String getName() {
         return name;
     }
-    
+
     public int getWins() {
         return wins;
     }
-    
+
     public int getLosses() {
         return losses;
     }
-    
+
     public int getKills() {
         return kills;
     }
-    
+
     public int getDeaths() {
         return deaths;
     }
